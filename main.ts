91c5153752db26ab96a65ef95942804712ef3095
Juan Garnica <juan@harbiz.io>
2024-10-30 15:39:29 +0100

    fix: update dev task in deno.json and simplify request handling in main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import { serveDir } from "@std/http/file-server";
 
 Deno.serve((req) => {
-  const pathname = new URL(req.url).pathname;
-
-  // Do dynamic responses
-  return new Response();
+    return serveDir(req, {
+      fsRoot: "./",
+    });
 });
